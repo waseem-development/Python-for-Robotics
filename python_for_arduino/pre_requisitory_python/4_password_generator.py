26aa2779e2d1714ce3395d7300f85698fdc8bbd6
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2025-12-30 19:54:03 +0500

    Finishing Password Generator

--- python_for_arduino/pre_requisitory_python/4_password_generator.py
+++ python_for_arduino/pre_requisitory_python/4_password_generator.py
@@ -3,38 +3,39 @@ import string as st
 
 def generatePassword(length):
     passStr = "" + st.ascii_lowercase
-    askUppercase = input("Do u want your password to contain uppercase letters (A-Z)? Type Y/N: ")
 
-    while askUppercase == "Y".lower() or askUppercase == "N".lower():
+    while True:
+        askUppercase = input("Do u want your password to contain uppercase letters (A-Z)? Type Y/N: ")
+
         if askUppercase == "Y".lower():
             passStr += st.ascii_uppercase
             break
         elif askUppercase == "N".lower():
             break
         else:
-            askUppercase = input("Invalid character please add Y/N: ")
+            print("Invalid character! Please enter valid character.")
             continue
     
-    askDigits = input("Do u want your password to contain digits (0-1)? Type Y/N: ")
-    while askDigits == "Y".lower() or askDigits == "N".lower():
+    while True:
+        askDigits = input("Do u want your password to contain digits (0-1)? Type Y/N: ")
         if askDigits == "Y".lower():
             passStr += st.digits
             break
         elif askDigits == "N".lower():
             break
         else:
-            askDigits = input("Invalid character please add Y/N: ")
+            print("Invalid character! Please enter valid character.")
             continue
     
-    askSpecialCharacters = input("Do u want your password to contain Special Characters (#@%^ etc)? Type Y/N: ")
-    while askSpecialCharacters == "Y".lower() or askSpecialCharacters == "N".lower():
+    while True:
+        askSpecialCharacters = input("Do u want your password to contain Special Characters (#@%^ etc)? Type Y/N: ")
         if askSpecialCharacters == "Y".lower():
             passStr += st.punctuation
             break
         elif askSpecialCharacters == "N".lower():
             break
         else:
-            askSpecialCharacters = input("Invalid character please add Y/N: ")
+            print("Invalid character! Please enter valid character.")
             continue
 
     password = "".join(rdm.choices(passStr, k=length))
@@ -44,16 +45,15 @@ while(True):
     length = int(input("Enter the length of your password: "))
     password = generatePassword(length)
     print("Your generated password: ", password)
-    again = input("Do you want to continue? Y/N: ")
-    if again == "Y".lower():
-        continue
-    elif again == "N".lower():
-        break
-    else: 
-        again = input("Invalid Character!!! Please input a valid character (Y/N): ")
+
+    while True:
+        again = input("Do you want to continue? Y/N: ")
         if again == "Y".lower():
-            continue
-        elif again == "N".lower():
             break
+        elif again == "N".lower():
+            exit()
+        else: 
+            print("Invalid character! Please enter Y or N.")
+
 
 
