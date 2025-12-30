4a4ffdf3046cb7f84a44fc6137673b4fafad89fb
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2025-12-30 12:31:11 +0500

    Created the half structure of password generator

---
+++ python_for_arduino/pre_requisitory_python/4_password_generator.py
@@ -0,0 +1,36 @@
+import random as rdm
+import string as st
+def generatePassword():
+    pass
+
+
+
+while(True):
+    length = int(input("Enter the length of your password: "))
+
+    # *********************************
+    askUppercase = input("Do u want to use capital letters? (Type Yy/Nn): ")
+    if askUppercase == "N" or askUppercase == "n":
+        pass
+    elif askUppercase == "Y" or askUppercase == "y":
+        pass
+    else: 
+        print("Error!!. Invalid input, please enter Yy/Nn:")
+
+    # *********************************
+    askDigits = input("Do u want to use digits? (Type Yy/Nn): ")
+    if askUppercase == "N" or askUppercase == "n":
+        pass
+    elif askUppercase == "Y" or askUppercase == "y":
+        pass
+    else: 
+        print("Error!!. Invalid input, please enter Yy/Nn:")
+    
+    # *********************************
+    askSpecialCharacters = input("Do u want to use digits? (Type Yy/Nn): ")
+    if askUppercase == "N" or askUppercase == "n":
+        pass
+    elif askUppercase == "Y" or askUppercase == "y":
+        pass
+    else: 
+        print("Error!!. Invalid input, please enter Yy/Nn:")
